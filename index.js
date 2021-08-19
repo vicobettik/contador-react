26666e2e242fa2be11dfe2c48a825a238b0819db
Victor Fuentes <vicuo@hotmail.com>
2021-08-18 20:09:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+    <Contador/>,
+    document.querySelector("#root")
+)
---
+++ serviceWorker.js
@@ -0,0 +1,57 @@
+const CACHE_ELEMENTS = [
+    "./",
+    "https://unpkg.com/react@17/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js",
+    "./style.css",
+    "./components/Contador.js"
+]
+
+const CACHE_NAME = "v1_cache_contador_react"
+
+self.addEventListener("install", (e) => {
+
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                cache.addAll(CACHE_ELEMENTS)
+                    .then(() => {
+                        self.skipWaiting()
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })
+    )
+});
+
+self.addEventListener("activate", (e) => {
+
+    const cacheWhitelist = [CACHE_NAME];
+
+    e.waitUntil(
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(cacheNames.map((cacheName) =>{
+                    cacheWhitelist.indexOf(cacheName) === -1 && caches.delete(cacheName)
+                }))
+            })
+            .then(() => {
+                self.clients.claim()
+            })
+    )
+});
+
+self.addEventListener("fetch", (e) => {
+
+    e.respondWith(
+        caches.match(e.request)
+            .then((res) => {
+                if (res) {
+                    return res
+                }
+                return fetch(e.request)
+            })
+    )
+    
+})
